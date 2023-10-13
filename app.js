241dac8ba9e94903138dcdc71e39765ea4fcb202
csm <csm009177@gmail.com>
2023-10-13 17:21:38 +0900

    make server in app.js

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(request, response){
-
+  console.log(request.method);
+  console.log(request.url);
+  let writeHeadObject = {
+    'Content-Type' : 'text/html'
+  }
+  response.writeHead(200, writeHeadObject);
   
-});
-
+  fs.readFile("index.html", function(err, data){
+    if (err) {
+      console.error('파일을 읽지 못했습니다');
+    } else {
+      response.end(data);
+    }
+  })
+}).listen(1234);
