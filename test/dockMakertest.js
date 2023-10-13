dfbc10e71d35b99015f6f469ff6a357536e1ded9
csm <csm009177@gmail.com>
2023-10-13 20:16:10 +0900

    get img.gif

---
+++ test/dockMakertest.js
@@ -0,0 +1,23 @@
+const dockMaker = require('./mod/dockMaker.js');
+
+
+// http 서버를 만들기 위해 내장 모듈인 http를 가져오기(import)하였다
+const http = require('http');
+const fs = require('fs');
+
+const contenType = require('./mod/contenType.js');
+console.log(contenType);
+
+let server = http.createServer(function(request, response){
+  console.log(request.method);
+  console.log(request.url);
+  
+
+  response.writeHead(200, contenType);
+  response.end(console.log(dockMaker('test', 'notion.so', 'test')));
+
+});
+
+server.listen(1234, function(){
+  console.log('서버 가동중 끄려면 Ctrl + C를 누르세요')
+})
---
+++ test/reqtest.js
@@ -0,0 +1,12 @@
+req = { 
+  hell  : 10,
+  heven : 20
+            }
+req2 = { 
+  jain :9,
+  cain : 11
+}
+
+console.log(this.hell)
+console.log(this.cain)
+
