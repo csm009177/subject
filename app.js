ef32cfcd04757ebdb3c13639bbe8f1352cae0162
csm <csm009177@gmail.com>
2023-10-16 15:36:31 +0900

    put patient img in img tag

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 // http 서버를 만들기 위해 내장 모듈인 http를 가져오기(import)하였다
 const http = require('http');
 // readFile을 하기 위한 모듈 파일 시스템을 import 했다 
-const fs = require('fs');``
+const fs = require('fs');
 
 const contenType = require('./mod/contenType.js');
 console.log(contenType);
 
 let server = http.createServer(function(request, response){
-  console.info(request.method)
-  console.log(request);
+  console.log(request.method)
   console.log(request.url);
 
-  if (request.url === "/") {
+  if (request.url === "/" && request.method === "GET") {
     fs.readFile("index.html", function(err, data){
       if (err) {
         console.error('파일을 읽지 못했습니다');
@@ -21,8 +20,8 @@ let server = http.createServer(function(request, response){
       }
     })
   }
-  if (request.url === "/hospital"){
-    fs.readFile("hospital.html", function(err, data){
+  if (request.url === "/hos"){
+    fs.readFile("hos.html", function(err, data){
       if (err) {
         console.error('파일을 읽지 못했습니다');
       } else {
