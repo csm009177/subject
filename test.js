3bd72960dc32c8ff05c8353633a080521b008f57
csm <csm009177@gmail.com>
2023-10-17 14:15:19 +0900

    try to readFile out of createServer

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
 const dockMaker = require('./mod/dockMaker.js');
-
+const fs = require('fs');
+const readHtml =  fs.readFile('index.html', function(err, data) {
+  if(err){
+    console.log('error');
+  } else{
+  response.log(data);
+  }
+})
 // http 서버를 만들기 위해 내장 모듈인 http를 가져오기(import)하였다
 const http = require('http');
 const fs = require('fs');
@@ -10,8 +17,7 @@ console.log(contenType);
 let server = http.createServer(function(request, response){
   console.log(request.method);
   console.log(request.url);
-  
-
+  readHtml
   response.writeHead(200, contenType);
   response.end((dockMaker('test', 'notion.so', 'notion')));
 
