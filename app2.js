14233fe2456132bba17c6bb2744aa62a63b78c77
csm <csm009177@gmail.com>
2023-10-18 10:07:04 +0900

    import custom contentType.js and dockMaker.js

--- app2.js
+++ app2.js
@@ -2,3 +2,8 @@
 const http = require('http');
 // import file systme module
 const fs = require('fs');
+
+// import custom contenType
+let contenType = require('./mod/contenType.js');
+// import custom dockMaker
+let dockMaker = require('./mod/dockMaker.js');
